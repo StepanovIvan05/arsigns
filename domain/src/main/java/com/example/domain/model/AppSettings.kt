a7954f11ad8f1b1cba0c1eabbdf38a35e50c6def
alexeyavdeenok <alexavdeenok@gmail.com>
2026-07-07 05:03:40 +0700

    replace models by float32

--- domain/src/main/java/com/example/domain/model/AppSettings.kt
+++ domain/src/main/java/com/example/domain/model/AppSettings.kt
@@ -5,9 +5,9 @@ enum class YoloModelType(
     val uiName: String,
     val inputSize: Int
 ) {
-    YOLO_V8_640("yolov8n_640_w8a16.tflite", "YOLOv8 640", 640),
-    YOLO_V8_416("yolov8n_416_w8a16.tflite", "YOLOv8 416", 416),
-    YOLO_V8_224("yolov8n_224_w8a16.tflite", "YOLOv8 224", 224);
+    YOLO_V8_640("yolov8n_640.tflite", "YOLOv8 640", 640),
+    YOLO_V8_416("yolov8n_416.tflite", "YOLOv8 416", 416),
+    YOLO_V8_224("yolov8n_224.tflite", "YOLOv8 224", 224);
 
     companion object {
         fun fromStoredValue(value: String?): YoloModelType {
